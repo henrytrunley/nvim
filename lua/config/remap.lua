ca53c6e4d5d008a90c3bdf51e49a9a82534e2b21
Henry Trunley <hjtrunley@gmail.com>
2025-09-03 15:16:23 +0100

    easy terminal open and escape shortcuts

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,6 +1,15 @@
-
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<leader>x", ":.lua<CR>")
 vim.keymap.set("v", "<leader>x", ":lua<CR>")
+
+vim.keymap.set("n", "<leader>t", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 15)
+  vim.cmd.startinsert()
+end
+)
+vim.keymap.set("t", "<Esc>", "<C-\\><C-N>")
