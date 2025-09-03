230852489c73851ccdf5a6b8fe0970313d1f3610
Henry Trunley <hjtrunley@gmail.com>
2025-09-03 16:25:13 +0100

    open terminal with ctrl+enter

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -4,7 +4,7 @@ vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<leader>x", ":.lua<CR>")
 vim.keymap.set("v", "<leader>x", ":lua<CR>")
 
-vim.keymap.set("n", "<leader>t", function()
+vim.keymap.set("n", "<C-Enter>", function()
   vim.cmd.vnew()
   vim.cmd.term()
   vim.cmd.wincmd("J")
